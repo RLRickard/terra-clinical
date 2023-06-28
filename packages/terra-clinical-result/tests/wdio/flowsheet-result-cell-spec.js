e1dc380d415f6a2271d74fd3a3d63942e560a78c
ry061521 <reagan.young@oracle.com>
2023-06-28 08:31:35 -0500

    add wdio

--- packages/terra-clinical-result/tests/wdio/flowsheet-result-cell-spec.js
+++ packages/terra-clinical-result/tests/wdio/flowsheet-result-cell-spec.js
@@ -64,4 +64,10 @@ Terra.describeViewports('FlowsheetResultCell', ['medium'], () => {
 
     Terra.validates.element('entered in error');
   });
+
+  it('displays a FlowsheetResultCell in a semantic table', () => {
+    browser.url('/#/raw/tests/terra-clinical-result/clinical-result/flowsheet-result-cell/semantic-table-with-flowsheet-result-cell');
+
+    Terra.validates.element('with semantic table');
+  });
 });
